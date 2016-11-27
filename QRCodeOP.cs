6ca06625ad2205dce55e6b2ae7eb8ab70814200c
jackletter <1286317554@qq.com>
2016-11-27 20:35:31 +0800

    整理

---
+++ QRCodeOP.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Util;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Web;
+using ThoughtWorks.QRCode.Codec.Data;
+
+namespace ImageCode
+{
+    public class QRCodeOP
+    {
+        #region 生成二维码图片(指定图片大小白边大小生成的图片的路径) public static void Encode(string str, int size, string filePath, int border = 0)
+        /// <summary>
+        /// 生成二维码图片
+        /// </summary>
+        /// <param name="str">内容字符串</param>
+        /// <param name="size">图片大小,图片宽度以像素为单位</param>
+        /// <param name="filePath">生成的二维码图片路径</param>
+        /// <param name="border">图片白边宽度,小于0表示无白边</param>
+        public static void Encode(string str, int size, string filePath, int border = 0)
+        {
+            System.Drawing.Image image = CreateQRCode(str, QRCodeEncoder.ENCODE_MODE.BYTE, QRCodeEncoder.ERROR_CORRECTION.M, 8, 5, size, border);
+            image.Save(filePath);
+        }
+        #endregion
+
+        #region 生成二维码图片 在中心位置会有小图标 public static void EncodeBack(string str, int size, string filePath, string backImgPath, int border = 0)
+        /// <summary>
+        /// 生成二维码图片 在中心位置会有小图标
+        /// </summary>
+        /// <param name="str">内容字符串</param>
+        /// <param name="size">图片大小,图片宽度以像素为单位</param>
+        /// <param name="filePath">生成的二维码图片路径</param>
+        /// <param name="backImgPath">中心小图标位置</param>
+        /// <param name="border">图片白边宽度,小于0表示无白边</param>
+        public static void EncodeBack(string str, int size, string filePath, string backImgPath, int border = 0)
+        {
+            System.Drawing.Image image = CreateQRCode(str, QRCodeEncoder.ENCODE_MODE.BYTE, QRCodeEncoder.ERROR_CORRECTION.M, 8, 5, size, border);
+            image = CombinImage(image, backImgPath);
+            image.Save(filePath);
+        }
+        #endregion
+
+        #region 合并两个图片 public static Image CombinImage(Image imgBack, string destImg)
+        /// <summary>    
+        /// 调用此函数后使此两种图片合并，类似相册，有个    
+        /// 背景图，中间贴自己的目标图片    
+        /// </summary>    
+        /// <param name="imgBack">作为背景的图片</param>    
+        /// <param name="destImg">要粘贴进来的图片的位置</param>    
+        public static Image CombinImage(Image imgBack, string destImg)
+        {
+            Image img = Image.FromFile(destImg);        //照片图片      
+            if (img.Height != 65 || img.Width != 65)
+            {
+                img = KiResizeImage(img, 65, 65, 0);
+            }
+            Graphics g = Graphics.FromImage(imgBack);
+
+            g.DrawImage(imgBack, 0, 0, imgBack.Width, imgBack.Height);      //g.DrawImage(imgBack, 0, 0, 相框宽, 相框高);     
+
+            //g.FillRectangle(System.Drawing.Brushes.White, imgBack.Width / 2 - img.Width / 2 - 1, imgBack.Width / 2 - img.Width / 2 - 1,1,1);//相片四周刷一层黑色边框    
+
+            //g.DrawImage(img, 照片与相框的左边距, 照片与相框的上边距, 照片宽, 照片高);    
+
+            g.DrawImage(img, imgBack.Width / 2 - img.Width / 2, imgBack.Width / 2 - img.Width / 2, img.Width, img.Height);
+            GC.Collect();
+            return imgBack;
+        }
+        #endregion
+
+        #region 生成二维码
+        /// <summary>
+        /// 生成二维码
+        /// </summary>
+        /// <param name="Content">内容文本</param>
+        /// <param name="QRCodeEncodeMode">二维码编码方式</param>
+        /// <param name="QRCodeErrorCorrect">纠错码等级</param>
+        /// <param name="QRCodeVersion">二维码版本号 0-40,一般使用8</param>
+        /// <param name="QRCodeScale">每个小方格的预设宽度（像素），正整数</param>
+        /// <param name="size">图片尺寸（像素），0表示不设置</param>
+        /// <param name="border">图片白边（像素），当size大于0时有效</param>
+        /// <returns></returns>
+        public static System.Drawing.Image CreateQRCode(string Content,
+            QRCodeEncoder.ENCODE_MODE QRCodeEncodeMode,
+            QRCodeEncoder.ERROR_CORRECTION QRCodeErrorCorrect,
+            int QRCodeVersion, int QRCodeScale, int size, int border)
+        {
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+            qrCodeEncoder.QRCodeEncodeMode = QRCodeEncodeMode;
+            qrCodeEncoder.QRCodeErrorCorrect = QRCodeErrorCorrect;
+            qrCodeEncoder.QRCodeScale = QRCodeScale;
+            qrCodeEncoder.QRCodeVersion = QRCodeVersion;
+            System.Drawing.Image image = qrCodeEncoder.Encode(Content, Encoding.Default);
+
+            #region 根据设定的目标图片尺寸调整二维码QRCodeScale设置，并添加边框
+            if (size > 0)
+            {
+
+                #region 当设定目标图片尺寸大于生成的尺寸时，逐步增大方格尺寸
+                while (image.Width < size)
+                {
+                    qrCodeEncoder.QRCodeScale++;
+                    System.Drawing.Image imageNew = qrCodeEncoder.Encode(Content);
+                    if (imageNew.Width < size)
+                    {
+                        image = new System.Drawing.Bitmap(imageNew);
+                        imageNew.Dispose();
+                        imageNew = null;
+                    }
+                    else
+                    {
+                        qrCodeEncoder.QRCodeScale--; //新尺寸未采用，恢复最终使用的尺寸
+                        imageNew.Dispose();
+                        imageNew = null;
+                        break;
+                    }
+                }
+                #endregion
+
+
+                #region 当设定目标图片尺寸小于生成的尺寸时，逐步减小方格尺寸
+                while (image.Width > size && qrCodeEncoder.QRCodeScale > 1)
+                {
+                    qrCodeEncoder.QRCodeScale--;
+                    System.Drawing.Image imageNew = qrCodeEncoder.Encode(Content);
+                    image = new System.Drawing.Bitmap(imageNew);
+                    imageNew.Dispose();
+                    imageNew = null;
+                    if (image.Width < size)
+                    {
+                        break;
+                    }
+                }
+                #endregion
+
+
+                #region 如果目标尺寸大于生成的图片尺寸，则为图片增加白边
+                if (image.Width <= size)
+                {
+
+                    #region 根据参数设置二维码图片白边的最小宽度
+                    if (border > 0)
+                    {
+                        while (image.Width <= size && size - image.Width < border * 2 && qrCodeEncoder.QRCodeScale > 1)
+                        {
+                            qrCodeEncoder.QRCodeScale--;
+                            System.Drawing.Image imageNew = qrCodeEncoder.Encode(Content);
+                            image = new System.Drawing.Bitmap(imageNew);
+                            imageNew.Dispose();
+                            imageNew = null;
+                        }
+                    }
+                    #endregion
+
+                    //当目标图片尺寸大于二维码尺寸时，将二维码绘制在目标尺寸白色画布的中心位置
+                    if (image.Width < size)
+                    {
+                        //新建空白绘图
+                        System.Drawing.Bitmap panel = new System.Drawing.Bitmap(size, size);
+                        System.Drawing.Graphics graphic0 = System.Drawing.Graphics.FromImage(panel);
+                        int p_left = 0;
+                        int p_top = 0;
+                        if (image.Width <= size) //如果原图比目标形状宽
+                        {
+                            p_left = (size - image.Width) / 2;
+                        }
+                        if (image.Height <= size)
+                        {
+                            p_top = (size - image.Height) / 2;
+                        }
+
+                        //将生成的二维码图像粘贴至绘图的中心位置
+                        graphic0.DrawImage(image, p_left, p_top, image.Width, image.Height);
+                        image = new System.Drawing.Bitmap(panel);
+                        panel.Dispose();
+                        panel = null;
+                        graphic0.Dispose();
+                        graphic0 = null;
+                    }
+                }
+                #endregion
+            }
+            #endregion
+
+            return image;
+        }
+        #endregion
+
+        /// <summary>    
+        /// Resize图片    
+        /// </summary>    
+        /// <param name="bmp">原始Bitmap</param>    
+        /// <param name="newW">新的宽度</param>    
+        /// <param name="newH">新的高度</param>    
+        /// <param name="Mode">保留着，暂时未用</param>    
+        /// <returns>处理以后的图片</returns>    
+        private static Image KiResizeImage(Image bmp, int newW, int newH, int Mode)
+        {
+            try
+            {
+                Image b = new Bitmap(newW, newH);
+                Graphics g = Graphics.FromImage(b);
+                // 插值算法的质量    
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(bmp, new Rectangle(0, 0, newW, newH), new Rectangle(0, 0, bmp.Width, bmp.Height), GraphicsUnit.Pixel);
+                g.Dispose();
+                return b;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static bool IsTrue() // 在Image类别对图片进行缩放的时候,需要一个返回bool类别的委托 
+        {
+            return true;
+        }
+    }
+}
+
+/** 调用示例:
+    string str = "为生成的二维码图像裁剪白边并调整为请求的高度";
+    string bakPath = "E:/MyDemoLib/Frame/ImageCode/res/back.ico";
+    QRCodeOP.Encode(str, 100, "c:\\1.png", 20);
+    QRCodeOP.EncodeBack(str, 200, "c:\\2.png", bakPath, 20);
+*/
